d7e6cbab076fd5b97b143b823bce2d9a885ae3ad
ViniBOYs <pedfor633@gmail.com>
2024-12-12 20:57:18 -0300

    +

--- app/url.py
+++ app/url.py
@@ -2,13 +2,11 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import *
 
-# Configuração do roteador
 router = DefaultRouter()
 router.register(r'motos', MotoView)
 router.register(r'partsmoto', PartsMotoView)
 router.register(r'users', UserView)
 
-# URLs da API
 urlpatterns = [
     path('api/', include(router.urls)),
 ]
